77a07f51975ddc4a6a7d06592e6646d2ed87439d
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-22 11:54:23 -0300

    feat(/ddd): first java class

---
+++ ddd/001/HelloWorld.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class HelloWorld
+{
+	public static void main (String [] args)
+	{
+		//System.out.println ("Hello, World");
+		//agora vamos fazer um dialogo
+		JOptionPane.showMessageDialog (null, "Hello, Duniya.");
+	}
+}
